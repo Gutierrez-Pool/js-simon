00a12bcab1a839b8be2cb10d2c13206c1b9debd3
Pool-Gutierrez <pool.gutierrezv@gmail.com>
2024-02-11 19:56:54 +0100

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 // Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
 // Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
 
-// Consigli
-// Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
-// Da quante ore è composto un giorno?
-// Da quanti minuti è composta un'ora?
-// Da quanti secondi è composto un minuto?
-// Da quanti millisecondi è composto un secondo?
-// Quanti millisecondi mi separano da domani alle 9:30?
-// Esiste un oggetto JS in grado di gestire le date?
-// Esistono dei metodi per trasformare una data in millisecondi?
+
 
 // Now
 function mostraOrologio() {
@@ -23,8 +15,8 @@ function mostraOrologio() {
     if(secondi < 10) { secondi = '0' + secondi; }
   
     document.getElementById("orologio").innerHTML = ore+':'+minuti+':'+secondi;
+
 }
-  
 setInterval(mostraOrologio, 1000);
 
 
@@ -34,6 +26,10 @@ const lezione = new Date ("February 12, 2024 09:30:00");
 console.log (lezione);
 
 document.getElementById("lezione").innerHTML = lezione;
+
+// Prova
+// const lezione = new Date ("February 11, 2024 19:52:00");
+
 
 
 // Countdown
@@ -55,14 +51,18 @@ const countdown = setInterval(function() {
 
     ore = ore % 24;
 
+
     if(ore < 10) { ore = '0' + ore; }
     if(minuti < 10) { minuti = '0' + minuti; }
     if(secondi < 10) { secondi = '0' + secondi; }
 
+
     document.getElementById("timer").innerHTML = ore+'h : '+minuti+'m : '+secondi+'s';
 
-    if (countdown < 0) {
+    if (temporimasto <= 0) {
+
         clearInterval (countdown);
+
         document.getElementById("timer").innerHTML = "Tempo esaurito";
     }
 
